64d696d5a00d4dab641f440c60701aa151969979
= <amit.joshiusa@gmail.com>
2021-04-27 21:22:34 -0500

    think i did ctc loss

--- test.py
+++ test.py
@@ -99,7 +99,7 @@ def train(encoder, loader, img_size, map_size, int_to_char, char_to_int, device)
     optimizer = optim.Adam(encoder.parameters(), lr=0.01)
     
     epoch = 0
-    while training_steps < 5:
+    while training_steps < 2:
         total_loss = None
         print("Starting epoch: " + str(epoch))
         for sample in loader:
