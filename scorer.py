2acb9566508af9900b84275bd5c6457d0f66cf75
= <amit.joshiusa@gmail.com>
2021-05-07 14:20:35 -0500

    trying this string_idx

--- scorer.py
+++ scorer.py
@@ -19,12 +19,14 @@ class Scorer(object):
         self.tokenizer = tokenizer
         self.device = device
         self.languageModel.to(self.device)
-        self.history = defaultdict(lambda: 0.0)
+        self.languageModel.eval()
+        self.history = defaultdict(0.0)
 
     def get_score(self, string):
         
         #print(string)
-        
+        string_idx = map(lambda x: self.vocab_map[x], string)
+        print (string_idx)
         tokenize_input = self.tokenizer.tokenize(string)
         #print ("tokenize_input: " + str(tokenize_input))
 
@@ -42,10 +44,33 @@ class Scorer(object):
             loss = 0
         else:
             loss = loss.item()
-        return -(len(tokenize_input) - 1) * loss, loss
+        return -(len(string_idx) - 1) * loss, loss
 
 
     def get_score_fast(self, strings):
         strings = [''.join(x) for x in strings]
         scores = [self.get_score(string)[0] for string in strings]
         return scores
+        '''strings = [''.join(x) for x in strings]
+        history_to_update = defaultdict(0.0)
+        scores = []
+        for string in strings:
+            if len(string) <= 2:
+                score, hidden_state = self.get_score(string)
+                scores.append(score)
+                history_to_update[string] = score
+            elif string in self.history:
+                history_to_update[string] = self.history[string]
+                scores.append(self.history[string][0])
+            elif string[:-1] in self.history:
+                score, hidden = self.history[string[:-1]]
+                history_to_update[string] = score - loss
+                input, grt = torch.LongTensor([self.vocab_map[string[-2]]]).view(1, 1).to(self.device), torch.LongTensor([self.vocab_map[string[-1]]]).to(self.device)
+                pred, hidden = self.rnn(input, hidden)
+                loss = self.criterion(pred.view(-1, pred.size(-1)), grt).item()
+                history_to_update[string] = (score-loss, hidden)
+                scores.append(score-loss)
+            else:
+                raise ValueError("%s not stored" % (string[:-1]))
+        self.history = history_to_update
+        return scores'''
